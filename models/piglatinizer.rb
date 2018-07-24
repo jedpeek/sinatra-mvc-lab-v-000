968c9e428b6f5658aaee6f6f3beec17e13f318e9
Jed Peek <jedpeek@yahoo.com>
2018-07-24 03:40:52 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,19 +1,3 @@
 class PigLatinizer
 
-  def translate(text)
-    alpha = ('a'..'z').to_a
-    vowels = %w[a e i o u]
-    consonants = alpha - vowels
-
-    if vowels.include?(text[0])
-      str + 'ay'
-    elsif consonants.include?(text[0]) && consonants.include?(text[1])
-      text[2..-1] + text[0..1] + 'ay'
-    elsif consonants.include?(str[0])
-      text[1..-1] + text[0] + 'ay'
-    else
-      text # return unchanged
-    end
-  end
-
 end
