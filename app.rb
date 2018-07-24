3287f854ac325f391f72a622bdc6350be9e0eb52
Jed Peek <jedpeek@yahoo.com>
2018-07-24 03:57:44 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
 
   post '/piglatinize' do
     pl = PigLatinizer.new
-    @pig_latinized_text = pl.translate(params[:user_input])
+    @pig_latinized_text = pl.piglatinize(params[:user_input])
     erb :results
   end
 end
