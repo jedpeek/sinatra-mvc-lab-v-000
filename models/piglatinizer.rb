d88dcd4952c8880ebdf771b08803cef339e790cd
Jed Peek <jedpeek@yahoo.com>
2018-07-24 03:45:01 +0000

    automatically backed up by learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,6 +1,13 @@
 class PigLatinizer
 
   def translate(text)
-    return "Working"
+    if word =~ (/\A[aeiou]/i)
+  word = word + 'ay'
+elsif word =~ (/\A[^aeiou]/i)
+  match = /\A[^aeiou]/i.match(word)
+  word = match.post_match + match.to_s + 'ay'
+end
+word
+end
   end
 end
